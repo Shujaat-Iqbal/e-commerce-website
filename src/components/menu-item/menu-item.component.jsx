df47758d5a3acf081bd418729172c3398c9583b1
Shujaat Iqbal <shojaatiqbal@gmail.com>
2020-06-06 21:44:36 +0500

    Updated App structure
    - Divided home-page into multiple components
    - used style property in menu-item which takes an object and provides css properties in camelCase.
    - TODO: Add comments on styles.

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,20 @@
+// React Import
+import React from 'react';
+
+// Styles Import
+import './menu-item.styles.scss';
+
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`${size} menu-item`}
+       style={{
+         backgroundImage: `url(${imageUrl})`
+       }}
+  >
+    <div className="content">
+      <h1 className="title">{ title }</h1>
+      <span className="subtitle">Shop Now</span>
+    </div>
+  </div>
+);
+
+export default MenuItem;
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,16 @@
+// React Import
+import React from 'react';
+
+// Styles Import
+import './homepage.styles.scss'
+
+// Component Import
+import Directory from '../../components/directory/directory.component';
+
+const HomePage = () =>(
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
