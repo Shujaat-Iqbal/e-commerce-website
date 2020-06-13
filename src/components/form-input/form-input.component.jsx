b4daa6bf8d7789db72ba5b86af2180ec15f8df62
Shujaat Iqbal <shojaatiqbal@gmail.com>
2020-06-14 00:12:49 +0500

    Added form input component
    - Created a form input component that will e used throughout the app.
    
    Signed-off-by: Shujaat Iqbal <shojaatiqbal@gmail.com>

---
+++ src/components/form-input/form-input.component.jsx
@@ -0,0 +1,26 @@
+// React Import
+import React from 'react';
+
+// Styles Import
+import './form-input.styles.scss';
+
+const FormInput = ({ handleChange, label, ...props }) => (
+  <div className='group'>
+    <input
+      className='form-input'
+      onChange={handleChange}
+      {...props}
+    />
+    {
+      label
+        ? (
+          <label className={`${ props.value.length ? 'shrink' : '' } form-input-label`}>
+            {label}
+          </label>
+        )
+        : null
+    }
+  </div>
+);
+
+export default FormInput;
