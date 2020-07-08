d5b1268f959254af55fcb43defe5301780d9b8b1
Shujaat Iqbal <shojaatiqbal@gmail.com>
2020-07-08 22:14:50 +0500

    [CartIcon] Integrated item count
    - Used reduce function to get quantity count of items in a cart.
    - Used that value to get displayed in top right icon.
    
    Signed-off-by: Shujaat Iqbal <shojaatiqbal@gmail.com>

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -12,13 +12,13 @@ import { ReactComponent as ShoppingIcon } from '../../assets/icons/shopping-bag.
 import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div
     className='cart-icon'
     onClick={toggleCartHidden}
   >
     <ShoppingIcon className='shopping-icon' />
-    <span className='item-count'> 0 </span>
+    <span className='item-count'>{itemCount}</span>
   </div>
 );
 
@@ -30,4 +30,18 @@ const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+/**
+ * maps required properties from state to our props which we can then use inside our component through connect
+ * @param {storeObject} state (destructured)
+ * It's basically a selector which by definition takes
+ * in a whole state oject and pulls of a small portion
+ * or a slice of that state
+ */
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce(
+    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
+    0 // initial accumulatedQuantity
+  )
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
