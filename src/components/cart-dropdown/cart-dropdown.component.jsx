726bd9670a596adbc40eb9c2e129ee868e8a46cd
Shujaat Iqbal <shojaatiqbal@gmail.com>
2020-07-08 20:19:16 +0500

    [CartItem][CartDropdown] Added & integrated cartItem component
    - Added styles & functionality for cart item component.
    - Integrated cart dropdown and added cart item component in it along with integration with store.
    
    Signed-off-by: Shujaat Iqbal <shojaatiqbal@gmail.com>

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,12 +6,30 @@ import './cart-dropdown.styles.scss';
 
 // Component Import
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
 
-const CartDropdown = () => (
+// Redux Import
+import { connect } from 'react-redux';
+
+const CartDropdown = ({ cartItems }) => (
   <div className='cart-dropdown'>
-    <div className='cart-items'></div>
+    <div className='cart-items'>
+      {
+        cartItems.map(cartItem => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))
+      }
+    </div>
     <CustomButton>GO TO THE CHECKOUT</CustomButton>
   </div>
 );
 
-export default CartDropdown;
+/**
+ * maps required properties from state to our props which we can then use inside our component through connect
+ * @param {storeObject} state (destructured)
+ */
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems: cartItems
+});
+
+export default connect(mapStateToProps)(CartDropdown);
