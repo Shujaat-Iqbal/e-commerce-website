2c4bdc6ed3c1c78ce993bf33bca95f5c3cdb808c
Shujaat Iqbal <shojaatiqbal@gmail.com>
2020-08-17 23:28:49 +0500

    Removed redux THUNK code since we no longer use it
    Signed-off-by: Shujaat Iqbal <shojaatiqbal@gmail.com>

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -17,25 +17,3 @@ export const fetchCollectionFailure = errorMsg => ({
   type: ShopActionTypes.FETCH_COLLECTIONS_FALIURE,
   payload: errorMsg
 });
-
-/**
- * A function using thunk which gives a function taking a dispatch
- * THUNK: thunk provides dispatch to th e functions passed not opjects
- */
-export const fetchCollectionStartAsync = () => {
-  return dispatch => {
-    // Colllection reference for collections
-    const collectionRef = firestore.collection('collections');
-
-    // Thunk operations along with asynchronous requests
-    dispatch(fetchCollectionStart());
-
-    // Normalizing snapshot data
-    collectionRef.get()
-      .then(snapshot => {
-        const collections = convertCollectionsSnapshotToMap(snapshot);
-        dispatch(fetchCollectionSuccess(collections));
-      })
-      .catch(error => dispatch(fetchCollectionFailure(error.message)));
-  }
-}
